0b023741359af1880b8e61b43f2a45f178f0a19d
Marios Kanellopoulos <mariosknl@outlook.com>
2023-08-10 15:09:31 +0300

    delete action created

--- app/(dashboard)/tickets/actions.js
+++ app/(dashboard)/tickets/actions.js
@@ -20,6 +20,24 @@ export async function addTicket(formData) {
 		user_email: session.user.email,
 	});
 
+	if (error) {
+		throw new Error("Could not add the new ticket");
+	}
+
+	revalidatePath("/tickets");
+	redirect("/tickets");
+}
+
+export async function deleteTicket(id) {
+	const supabase = createServerActionClient({ cookies });
+
+	// delete the data
+	const { error } = await supabase.from("Tickets").delete().eq("id", id);
+
+	if (error) {
+		throw new Error("Could not delete the ticket");
+	}
+
 	revalidatePath("/tickets");
 	redirect("/tickets");
 }
