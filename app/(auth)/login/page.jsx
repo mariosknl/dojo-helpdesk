f5965a95472d836225822b92a6ec01b698311e6b
Marios Kanellopoulos <mariosknl@outlook.com>
2023-08-10 12:22:44 +0300

    making the auth forms

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,7 +1,19 @@
+"use client";
+
+import AuthForm from "../AuthForm";
+
 export default function Login() {
+	const handleSubmit = async (e, email, password) => {
+		e.preventDefault();
+
+		console.log("user login", email, password);
+	};
+
 	return (
 		<main>
 			<h2 className="text-center">Log in</h2>
+
+			<AuthForm handleSubmit={handleSubmit} />
 		</main>
 	);
 }
