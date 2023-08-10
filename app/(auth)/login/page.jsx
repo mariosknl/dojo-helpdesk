1e5fe40e7d3a4ae84df20bdfb3aec1a61d6adbe4
Marios Kanellopoulos <mariosknl@outlook.com>
2023-08-10 12:57:12 +0300

    logging user in & out

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,12 +1,33 @@
 "use client";
 
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useState } from "react";
+
+// components
 import AuthForm from "../AuthForm";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
+	const [error, setError] = useState("");
+	const router = useRouter();
+
 	const handleSubmit = async (e, email, password) => {
 		e.preventDefault();
+		setError("");
 
-		console.log("user login", email, password);
+		const supabase = createClientComponentClient();
+		const { error } = await supabase.auth.signInWithPassword({
+			email,
+			password,
+		});
+
+		if (error) {
+			setError(error.message);
+		}
+
+		if (!error) {
+			router.push("/");
+		}
 	};
 
 	return (
@@ -14,6 +35,7 @@ export default function Login() {
 			<h2 className="text-center">Log in</h2>
 
 			<AuthForm handleSubmit={handleSubmit} />
+			{error && <div className="error">{error}</div>}
 		</main>
 	);
 }
